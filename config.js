01e3b4057e41a168fac60e08b8610cd975bf287c
Rudragupta8777 <rudragupta1204@gmail.com>
2025-03-01 16:46:56 +0530

    final commit

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 export const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-};
+    apiKey: "AIzaSyBq5qoIGKWWq8NcVZqD9j_bVBMwvOlxhDY",
+    authDomain: "campusevents-e9eca.firebaseapp.com",
+    projectId: "campusevents-e9eca",
+    storageBucket: "campusevents-e9eca.appspot.com",
+    messagingSenderId: "165313818020",
+    appId: "1:165313818020:web:9278f19425e0aea393a531",
+    measurementId: "G-WYVFFRC5GF",
+  };
   
-export const CLOUD_NAME = process.env.NEXT_PUBLIC_CLOUDINARY_NAME;
-export const UPLOAD_PRESET = process.env.NEXT_PUBLIC_CLOUDINARY_PRESET;
+  // Cloudinary Configuration
+export const CLOUD_NAME = "dd3fpzqcu";
+export const UPLOAD_PRESET = "vit_events_upload";
   
